23d2cfb174e8142a4683079778f53fea8c15ddfe
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2021-06-28 09:50:45 +0900

    無駄にCPUループさせるようにコードを追加

--- loop.go
+++ loop.go
@@ -11,7 +11,15 @@ func main() {
 		file = os.Args[1]
 	}
 	fmt.Println("check file: " + file)
+	min := -2147483648
+	max := 2147483647
+	count := 0
 	for {
+		for i := min; i < max; i++ {
+			// do nothing
+		}
+		fmt.Println(count)
+		count = count + 1
 		_, err := os.Stat(file)
 		if err == nil {
 			break
